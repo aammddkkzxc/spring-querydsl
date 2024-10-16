50953b140bb3e225172d778315fd638062201d23
aammddkkzxc <ideal0427@naver.com>
2024-10-17 00:40:53 +0900

    feat: 스프링 데이터 페이징 활용2 - CountQuery 최적화 - 함수이기 때문에 구문이 실행이 안됨 - 조건 보고 호출 (컨텐츠 사이즈가 페이지 사이즈 보다 작거나.. 마지막 페이지 거나 하면 호출 x) - deprecated라서 fetchCount()안쓰고 그냥 쿼리로 함수 형태로 제공하면 될듯

--- src/main/java/study/querydsl/repository/MemberQuerydslRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberQuerydslRepositoryImpl.java
@@ -2,14 +2,17 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
 
 import java.util.List;
 
@@ -97,7 +100,7 @@ public class MemberQuerydslRepositoryImpl implements MemberQuerydslRepository {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -105,7 +108,7 @@ public class MemberQuerydslRepositoryImpl implements MemberQuerydslRepository {
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe()))
-                .fetchCount();
+                ;
 
 
 //        Long count = queryFactory
@@ -116,7 +119,8 @@ public class MemberQuerydslRepositoryImpl implements MemberQuerydslRepository {
 //        List<MemberTeamDto> content = results.getResults();
 //        long total = results.getTotal();
 
-        return new PageImpl<>(results, pageable, total);
+        return PageableExecutionUtils.getPage(results, pageable, () -> countQuery.fetchCount());
+//        return new PageImpl<>(results, pageable, total);
     }
 
 }
