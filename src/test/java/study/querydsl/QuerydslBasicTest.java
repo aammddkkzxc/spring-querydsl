3755bb9a4cb82171e703c97212ec1555bfaf9062
aammddkkzxc <ideal0427@naver.com>
2024-10-16 17:28:58 +0900

    feat: 프로젝션과 결과 반환 - @QueryProjection - constructor방식과 유사하지만 생성자 방식은 지정된 인자 외의 값들이 들어와도 컴파일 시점에 알지 못하는 반면 @QueryProjection은 가능 - Q파일이 생기고, dto가 querydsl에 의존성을 갖게 된다는 단점이 있음 - dto는 서비스, 컨트롤러, 심지어 api로 반환하는 형태로 쓰이기 때문에 여러 레이어에 걸쳐 돌아다니게 됨 -> 쿼리dsl의존성을 가진채로 돌아다닌다는 뜻 - 아키텍쳐 설계 방식에 따라 실용적으로 그냥 쓰기도 함

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -534,6 +535,18 @@ public class QuerydslBasicTest {
 
         for (UserDto userDto : result) {
             System.out.println(userDto);
+        }
+    }
+
+    @Test
+    void findDtoByProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println(memberDto);
         }
     }
 }
